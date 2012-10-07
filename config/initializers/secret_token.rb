7fa5bbb80fde8a5438bffec82b993477121b7f89
Kevin Hwang <k3vinhwang@gmail.com>
2012-10-07 13:58:24 -0700

    Fresh start! Beginning with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apricot::Application.config.secret_token = 'caf17ad7b77dd7351784e4560b6fdba07828b7feebc51bab36e44911b0d4fc1fa9b886e8971b12ac069b36a5d3a569236133fd0cf36b58dbd22eebf7efb7f818'
