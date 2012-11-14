933adf076a06ddd59b687cd4f70d0f631bc58821
Kevin Hwang <k3vinhwang@gmail.com>
2012-11-13 23:16:42 -0800

    Use acts_as_taggable rather than trying to roll my own solution

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails-backbone'
 
 gem 'haml-rails'
 
+gem 'acts-as-taggable-on', '~> 2.3.1'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121031071800) do
+ActiveRecord::Schema.define(:version => 20121114071353) do
 
   create_table "images", :force => true do |t|
     t.string   "url"
@@ -23,14 +23,21 @@ ActiveRecord::Schema.define(:version => 20121031071800) do
     t.datetime "data_updated_at"
   end
 
-  create_table "items", :force => true do |t|
-    t.text  "name"
-    t.float "price"
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
   end
 
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
   create_table "tags", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string "name"
   end
 
 end
